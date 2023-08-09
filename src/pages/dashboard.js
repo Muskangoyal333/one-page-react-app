ec53f64b098d37e0307c6fb178a0f1852126bd6b
Muskangoyal333 <106796343+Muskangoyal333@users.noreply.github.com>
2023-08-09 12:20:40 +0530

    Name changed in dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Dashboard=()=>{
     return(
         <div>
-            <h1>Dashboard Muskan</h1>
+            <h1>Dashboard Shashank</h1>
         </div>
     )
 };
